9a7f01455cc951938048fbc844c591bc7414b8ed
Serhii Kotliar <s.kotliar@student.csn.khai.edu>
2021-06-30 08:52:21 +0300

    added 1365 problem-solution

---
+++ LeetCodeTasks/SmallerThanCurrentNumber_1365/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SmallerThanCurrentNumber_1365
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new();
+            var result = solution.SmallerNumbersThanCurrent(new int[] { 8, 1, 2, 2, 3 });
+            foreach (var i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ LeetCodeTasks/SmallerThanCurrentNumber_1365/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallerThanCurrentNumber_1365
+{
+    public class Solution
+    {
+        public int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            Dictionary<int, int> numsMap = new();
+            var sortedNums = nums.OrderByDescending(x => x).ToArray();
+
+            for (int i = 0; i < sortedNums.Length; i++)
+            {
+                int tmp = sortedNums.Length - (i + 1);
+                if (numsMap.ContainsKey(sortedNums[i]))
+                {
+                    numsMap[sortedNums[i]] = tmp;
+                }
+                else
+                {
+                    numsMap.Add(sortedNums[i], tmp);
+                }
+            }
+
+            return nums.Select(x => numsMap[x]).ToArray();
+        }
+    }
+}
